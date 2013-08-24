da204d8966a52ad55051947cb7e78d436107e840
Luke Pomfrey <luke@lukepomfrey.org>
2013-08-24 18:18:58 +0100

    Update __version__ declaration in __init__

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__init__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from distutils import version
+
+
 __version__ = '{{ cookiecutter.version }}'
+version_info = version.StrictVersion(__version__).version
